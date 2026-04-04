8fc86731ae226550d63c048f6bc6c2c14a8084ee
Zintrall <jenik.tesacek@seznam.cz>
2026-04-04 21:47:50 +0200

    fixed vial

--- Firmware/keymaps/vial/config.h
+++ Firmware/keymaps/vial/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define ENCODERS_PAD { GP28, GP27 }
+#define ENCODERS_PAD { GP27, GP28 }
 
 #define VIAL_KEYBOARD_UID {0xD0, 0x6F, 0xBF, 0xD0, 0xFD, 0xC3, 0x99, 0x98}
 
--- Firmware/keymaps/vial/keymap.c
+++ Firmware/keymaps/vial/keymap.c
@@ -12,10 +12,8 @@ enum layer_names {
 };
 
 bool oled_task_user(void) {
-    // Vypíše slovo "Layer "
     oled_write_P(PSTR("Layer "), false);
 
-    // Zjistí číslo aktuální vrstvy a převede ho na text
     oled_write(get_u8_str(get_highest_layer(layer_state), ' '), false);
 
     return false;
@@ -23,7 +21,7 @@ bool oled_task_user(void) {
 
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_BASE]   = { ENCODER_CCW_CW(KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP) },
+    [_BASE]   = { ENCODER_CCW_CW(KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN) },
     [_LAYER1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [_LAYER2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
     [_LAYER3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
@@ -31,19 +29,19 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_P7, KC_P8, KC_P9,
-        KC_P4, KC_P5, KC_P6
+        KC_7, KC_8, DF(3),
+        KC_4, KC_5, KC_6
     ),
     [_LAYER1] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_P1,   KC_P2,   KC_P3
+        KC_A, KC_B, DF(3),
+        KC_C, KC_D, KC_E
     ),
     [_LAYER2] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_A,    KC_B,    KC_C
+        KC_X, KC_Y, DF(3),
+        KC_Z, KC_R, KC_D
     ),
     [_LAYER3] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_D,    KC_E,    KC_F
+        DF(0), DF(1), DF(2),
+        KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
